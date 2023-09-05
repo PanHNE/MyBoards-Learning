552ca4b120f517b295e32f04651e9a309f0d5e00
Adrian Domin <adriandominx@gmail.com>
2023-09-05 11:20:41 +0200

    Format file

--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -7,15 +7,17 @@
         public string Area { get; set; }
         public string IterationPath { get; set; }
         public int Priority { get; set; }
+
         // Epic
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+
         // Issue
         public decimal Efford { get; set; }
+
         // Task
         public string Activity { get; set; }
         public decimal RemaningWork { get; set; }
-
         public string Type { get; set; }
     }
 }
