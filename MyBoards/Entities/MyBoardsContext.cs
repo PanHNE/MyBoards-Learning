d76acb7352f5546dc20b8578889ff987fcd44a69
Adrian Domin <adriandominx@gmail.com>
2023-09-05 11:21:33 +0200

    Addproperties for table WorkItem and Comment by setting them in method OnModelCreating

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -13,5 +13,34 @@ namespace MyBoards.Entities
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.State)
+                .IsRequired();
+
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.Area)
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(wi => wi.State).IsRequired();
+                eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
+                eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+                eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2");
+                eb.Property(wi => wi.EndDate).HasPrecision(3);
+                eb.Property(wi => wi.Activity).HasMaxLength(200);
+                eb.Property(wi => wi.Activity).HasPrecision(14, 2);
+                eb.Property(wi => wi.Priority).HasDefaultValue(1);
+            });
+
+            modelBuilder.Entity<Comment>(eb =>
+            {
+                eb.Property(x => x.CreatedDate).HasDefaultValueSql("getutcdate()");
+                eb.Property(x => x.UpdatedDate).ValueGeneratedOnUpdate();
+            });
+        }
     }
 }
