33750b5a6144818c5d6927f8b7513f9bdc9f4c35
Adrian Domin <adriandominx@gmail.com>
2023-09-07 12:51:41 +0200

    Add 2 another element to table WorItemState by writing code in a new migration file

---
+++ MyBoards/Migrations/20230907103557_AdditionWorkItemStateSeedByManual.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class AdditionWorkItemStateSeedByManual : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "WorkItemState",
+                column: "Value",
+                value: "OnHold"
+                );
+
+            migrationBuilder.InsertData(
+                table: "WorkItemState",
+                column: "Value",
+                value: "Rejected"
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemState",
+                keyColumn: "Value",
+                keyValue: "OnHold"
+                );
+
+            migrationBuilder.DeleteData(
+                table: "WorkItemState",
+                keyColumn: "Value",
+                keyValue: "Rejected"
+                );
+
+        }
+    }
+}
